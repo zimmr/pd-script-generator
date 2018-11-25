fc5af0efccef4dc47bca545ec2b73ff91c7a541f
João Zimmer <joaozimmr@gmail.com>
2018-11-25 16:44:38 -0200

    fixed formatting and cleaned up a bit

--- pdscriptgen.py
+++ pdscriptgen.py
@@ -1,15 +1,20 @@
 import sys
 import regex
 
-def assign_to_fc(d,k,x,y):
+def assign_to_fc(d, k, x, y):
     key = k
     d[key] = [x, (y, z)]
     return d[key]
 
-
 def convert_bpm_to_ms(x):
     return x / 15000
 
+def evaluate_patterns(x):
+    toPrint = []
+    if e == '\w\(\)':
+        functions(e)
+    else:
+        toPrint.append(e)
 
 def print_gap(gap, x, y):
     if x == y:
@@ -27,8 +32,7 @@ with open(sys.argv[1]) as input:
         patterns[key] = line[2:len(line)]
 
 
-#arrangeInput = open(sys.argv[2], "r")
-#arrange = arrangeInput.readline()
+input.close()
 
 
 # instrument tag and paremeters
@@ -37,24 +41,23 @@ with open(sys.argv[2]) as f:
     bpm = int(regex.search('bpm ?= ?(\d*)', read_data).group('1'))
     first = list(patterns.keys())[0]
     gap = convert_bpm_to_ms(bpm)
-    regExp = regex.findall('(n?seq):(\w*)\n\t?(r|v|s|n|o)? ?=? ?(.*)\n?\t?(r|v|s|n|o)? ?=? ?(.*)\n?\t?(r|v|s|n|o)? ?=? ?(.*)\n?', read_data)
+    regExp = regex.findall('(n?seq):(\w*)\n\t?(r|v|s|n|o)? ?=? ?(.*)\n?\t?(r|v|s|n|o)? ?=? ?(.*)\n?\t?(r|v|s|n|o)? ?=? ?(.*)\n?', read_data) # this regex can be shortened
     arrangement = {}
     for i in regExp:
         x = regExp[i]
-        for j in range(2,6,2):
-            # faltou testar se as strings são válidas
+        for j in range(2, 6, 2):
+            # verify if the strings are not null
             tsil = []
-            tsil.append((x[j],[x[j+1]]))
+            tsil.append((x[j], [x[j+1]]))
         assign_to_fc(arrangement, x[1], x[0], tsil)
         tsil.clear()
 
+f.close()
 
-#instruments = arrangement.keys()
-#arrangement = {}
-sizes = []
-tsil = []
 
 # preparing stored patterns
+sizes = []
+tsil = []
 for i in arrangement:
     ev = arrangement[i]
     it = ev(1)
@@ -62,14 +65,14 @@ for i in arrangement:
         at = it[j]
         test = 1
         while test:
-            exp = regex.findall("(?'head'(\w*\d*\s*)*(?!\*))(?'multi'\d*)\*\[(?'inner'(\w*\d*\s*)*(?:[^[]]|(?R))*)\](?'tail'(\w*\d*\s*)*(?!\*))",at[1]) 
+            exp = regex.findall("(?'head'(\w*\d*\s*)*(?!\*))(?'multi'\d*)\*\[(?'inner'(\w*\d*\s*)*(?:[^[]]|(?R))*)\](?'tail'(\w*\d*\s*)*(?!\*))", at[1]) 
             for k in exp:
                 at[1] = exp.group('head') + (exp.group('multi') * exp.group('inner')) + exp.group('tail')
                 brackets = regex.search('(\[|\])', pattern)
                 if brackets == None:
                     test = 0
                     break
-        result = regex.findall('(\S*[^\s])',at[i])
+        result = regex.findall('(\S*[^\s])', at[i])
         for i in range(len(result)):
             tsil.append(result[i])
         at[1] = tsil
@@ -77,7 +80,8 @@ for i in arrangement:
         tsil.clear()
 
 
-with open(sys.argv(2),w) as output:
+# writing patterns to output file
+with open(sys.argv(2), w) as output:
     for m in range(max(sizes)):
         for i in arrangement: 
             ev = arrangement[i]
@@ -93,39 +97,40 @@ with open(sys.argv(2),w) as output:
                        if at[0l == 'r':
                            paramTag = 'pattern'
                            if patternCall[m] == 'ø':
-                               patternToPrint = '0'*16
+                               patternToPrint = '0' * 16
                            else:
                                patternToPrint = patterns[i]
-                       elif at[0] == '\v':
+                       elif at[0] == 'v':
                            paramTag = 'vpattern'
                            if patternCall[m] == 'ø':
-                               patternToPrint = '100'*16
+                               patternToPrint = '100' * 16
                            else:
                                patternToPrint = patterns[i]
                        elif at[0] == 's':
                            paramTag = 'rpattern'
                            if patternCall[m] == 'ø':
-                               patternToPrint = '0'*16
+                               patternToPrint = '0' * 16
                            else:
                                patternToPrint = patterns[i]
                    elif ev[0] == nseq:
-                       if at[0l == '\n':
+                       if at[0l == 'n':
                            paramTag = 'note-pattern'
                            if patternCall[m] == 'ø':
-                               patternToPrint = '0'*16
+                               patternToPrint = '0' * 16
                            else:
                                patternToPrint = patterns[i]
-                       elif at[0] == '\o':
+                       elif at[0] == 'o':
                            paramTag = 'oct-pattern'
                            if patternCall[m] == 'ø':
-                               patternToPrint = '4'*16
+                               patternToPrint = '4' * 16
                            else:
                                patternToPrint = patterns[i]
-                       elif at[0] == '\v':
+                       elif at[0] == 'v':
                            paramTag = 'vel-pattern'
                            if patternCall[m] == 'ø':
-                               patternToPrint = '100'*16
+                               patternToPrint = '100' * 16
                            else:
                                patternToPrint = patterns[i]
-                   output.write("{0} {1}-{2} {3};".format(gapToPrint,paramTag,instrName,patternToPrint))   
+                   output.write("{0} {1}-{2} {3};".format(gapToPrint, paramTag, instrName, patternToPrint))   
 
+output.close()
